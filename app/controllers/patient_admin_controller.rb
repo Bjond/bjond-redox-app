853a2dcf8070aba17d384b0d3df8c09bb0737806
blake <blake.rego@gmail.com>
2016-08-01 19:47:45 -0400

    Redox endpoint will now transfer event data to fire event in Bjond Server core. For now, this is limited to visitNumber info.

--- app/controllers/patient_admin_controller.rb
+++ app/controllers/patient_admin_controller.rb
@@ -2,9 +2,22 @@ class PatientAdminController < ApplicationController
 
   skip_before_filter :verify_authenticity_token, :only => [:arrival]
 
+  require 'bjond-api'
+
   def arrival
     # Handles payload from Redox, relays to Bjond Server Core in form of event.
     puts request.raw_post
+    parsed = JSON.parse(request.raw_post)
+    meta_info = parsed["Meta"]
+    visit_info = parsed["Visit"]
+    patient_info = parsed["Patient"]
+    visit_number = visit_info["VisitNumber"]
+    event_data = {
+      :visitNumber => visit_number
+    }
+    BjondRegistration.all.each do |r|
+      BjondApi::fire_event(r, event_data)
+    end
     render :json => {
       :status => 'OK'
     }
