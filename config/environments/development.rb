d3887c2325662c862e71101f1b002d61ebb3707b
blake <blake.rego@gmail.com>
2016-08-06 20:45:24 -0400

    Modifying host url handling in service definition.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,10 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  config.action_controller.default_url_options = {:host => "localhost:3001"}
+  config.action_controller.default_url_options = {
+    :host => "localhost:3001",
+    :full_url => "http://localhost:3001"
+  }
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
